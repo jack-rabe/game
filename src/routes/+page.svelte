0733c32ccdf02b3d6a9f525cd416bfc46dc015d4
Jack Rabe <jhrabe@wisc.edu>
2023-08-28 14:11:08 -0500

    add daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <h1 class="bg-red-300">Welcome to SvelteKit</h1>
+<button class="btn"> btn </button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style global>
